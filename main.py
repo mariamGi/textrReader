454c9d3ae3b382f345b1eda20f32252510c542ba
mariam <javaxishvilimariami88@Gmail.com>
2021-11-17 00:24:26 +0400

    wrote code which read file

---
+++ main.py
@@ -0,0 +1,9 @@
+# this function open and read file,split and return some word as list from txt file
+def text_list(text):
+    with open(text, 'r') as reader:
+        line = reader.readlines()[-1]
+    line_list = line.split(",")
+    return line_list[:6]
+
+
+print(text_list(text='sample2.txt'))
